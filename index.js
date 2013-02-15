5d38b7b71b637681bbd761aab390db68ff4939c7
coreindustries <cszopinski@cpbgroup.com>
2013-02-15 10:54:53 -0800

    troubleshooting a null response

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var cosm = require('cosm'),
     cosm_api_key = 'enter key here',        // update this
     cosm_data_feed = 'enter feed id here',  // update this
     url = 'google.com',                     // update this
-    check_frequency = 5,                    // seconds
+    check_frequency = 2,                    // seconds
     client = new cosm.Cosm(cosm_api_key),
     feed = new cosm.Feed(cosm, {id: cosm_data_feed}),
     stream = new cosm.Datastream(client, feed, {id: 1}),
-    sys = require('sys'),
+    sys = require('util'),
     exec = require('child_process').exec,
     child;
 
@@ -18,7 +18,8 @@ setInterval((function() {
 
 
 function checkResponse(){
-   child = exec('ping -c 1 '+url,
+    var cmd = 'ping -c 1 '+url;
+   child = exec(cmd,
       function (error, stdout, stderr) {
 
         var p = extract(stdout);
@@ -45,12 +46,13 @@ function checkResponse(){
 // http://www.w3schools.com/jsref/jsref_obj_regexp.asp
 
 function extract(str){
+    console.log(str);
     // var pattern = /^([0-9]+) bytes from ([0-9.]+): icmp_seq=([0-9]+) ttl=([0-9]+) time=([0-9.]+) ms/m; // mac ping format
     var pattern = /^(\d+) bytes from (\d+-\d+-\d+-\d+.\w+.\w+) \((\d+.){4}\): icmp_req=(\d+) ttl=(\d+) time=(\d+.\d) ms/m; // raspberry pi ping format
     var regex = new RegExp(pattern);
     result = regex.exec(str);
+    console.log(result);
     return result;
-    // console.log(result);
 }
 
 
