8508c0bcbdecbcc140215d22badd8559f4191daf
coreindustries <corey@core-industries.com>
2013-02-15 11:12:47 -0800

    rewrote the regex to be much simpler

--- index.js
+++ index.js
@@ -18,40 +18,39 @@ setInterval((function() {
 
 
 function checkResponse(){
-    var cmd = 'ping -c 1 '+url;
+   var cmd = 'ping -c 1 '+url;
    child = exec(cmd,
       function (error, stdout, stderr) {
 
         var p = extract(stdout);
+	// console.log(p);
 
-        sys.print('stderr: ' + stderr);
+        //sys.print('stderr: ' + stderr);
         if (error !== null) {
           console.log('exec error: ' + error);
         }else{
-            stream.addPoint(p[6]);  // make this 5, if used on a mac
+          console.log(url+ " in "+p[1]+" ms");
+	  stream.addPoint(p[1]);
         }
 });
 }
 
+//PING google.com (74.125.224.231) 56(84) bytes of data.
+//64 bytes from lax04s08-in-f7.1e100.net (74.125.224.231): icmp_req=1 ttl=57 time=4.41 ms
 
+//--- google.com ping statistics ---
+//1 packets transmitted, 1 received, 0% packet loss, time 0ms
+//rtt min/avg/max/mdev = 4.416/4.416/4.416/0.000 ms
 
-// 64 bytes from 24.28.199.168: icmp_seq=0 ttl=51 time=72.211 ms
-
-// --- rr.com ping statistics ---
-// 1 packets transmitted, 1 packets received, 0.0% packet loss
-// round-trip min/avg/max/stddev = 72.211/72.211/72.211/0.000 ms
-
-// extract(str);
-
-// http://www.w3schools.com/jsref/jsref_obj_regexp.asp
 
 function extract(str){
-    console.log(str);
+    // console.log(str);
     // var pattern = /^([0-9]+) bytes from ([0-9.]+): icmp_seq=([0-9]+) ttl=([0-9]+) time=([0-9.]+) ms/m; // mac ping format
-    var pattern = /^(\d+) bytes from (\d+-\d+-\d+-\d+.\w+.\w+) \((\d+.){4}\): icmp_req=(\d+) ttl=(\d+) time=(\d+.\d) ms/m; // raspberry pi ping format
+    // var pattern = /^(\d+) bytes from (\d+-\d+-\d+-\d+.\w+.\w+) \((\d+.){4}\): icmp_req=(\d+) ttl=(\d+) time=(\d+.\d) ms/m; // raspberry pi ping format
+    var pattern = /time=(\d+.\d.)/;
     var regex = new RegExp(pattern);
     result = regex.exec(str);
-    console.log(result);
+    //console.log(result);
     return result;
 }
 
